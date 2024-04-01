91a7d6653a470577e68e9a2bd9f1fba383cb385a
Sandhya Bairi <sandhya.bairi154@gmail.com>
2024-04-01 17:22:27 +0530

    able to return object instead of a string using Pydantic

--- ice_breaker.py
+++ ice_breaker.py
@@ -9,13 +9,12 @@ from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 from third_parties.twitter_with_stubs import scrape_user_tweets
+from output_parsers import person_intel_parser
 
 name = "Sandhya Rani Bairi"
 
-if __name__ == "__main__":
 
-    print("Hello LangChain!")
-
+def ice_break(name: str) -> str:
     linkedin_profile_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
 
@@ -28,18 +27,30 @@ if __name__ == "__main__":
         2. two interesting facts about them
         3. A topic that may interest them
         4. 2 creative ice breakers to open a conversation with them
+        \n{format_instructions}
         """
 
     summary_prompt_template = PromptTemplate(
         input_variables=["linkedin_information", "twitter_information"],
         template=summary_template,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
-    # res = chain.invoke(input={"information": information})
+    result = chain.run(linkedin_information=linkedin_data, twitter_information=tweets)
 
-    print(chain.run(linkedin_information=linkedin_data, twitter_information=tweets))
+    print(result)
+    return person_intel_parser.parse(result)
+
+
+if __name__ == "__main__":
+
+    print("Hello LangChain!")
+
+    result = ice_break(name="Sandhya Rani Bairi")
 
     # print(chain.invoke(input={"linkedin_information": linkedin_data, "twitter_information":tweets}))
